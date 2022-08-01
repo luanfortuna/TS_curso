96e33006f50b10f7e82c4f3b6596150de9df8178
Luan Fortuna <luan395@gmail.com>
2022-07-31 21:05:38 -0300

    alteracao 1

--- src/get_set.ts
+++ src/get_set.ts
@@ -1,23 +1,23 @@
 
 // Caso de uso de um JOGO
 
-class Jogo{
+class Jogo {
   private servidor: string;
   private id: string = "1234";
 
-  constructor(servidor: string){
+  constructor(servidor: string) {
     this.servidor = servidor;
   }
 
   //Metodo GET = Para pegar o valor ao inves de acessar diretamente a propriedade
-  get getServidorIP(){
+  get getServidorIP() {
     console.log("== METODO GET ==")
     return this.servidor;
   }
 
   //Metodo SET = Passar / alterar algum atributo.
-  set setServidorIP(novoIp: string){
-    if(this.servidor === novoIp){
+  set setServidorIP(novoIp: string) {
+    if (this.servidor === novoIp) {
       throw new Error("O novo IP deve ser diferente do IP antigo.")
     }
 
@@ -27,14 +27,5 @@ class Jogo{
 }
 
 
-const GTA5 = new Jogo("192.168.5.10");
 
-try{
-  GTA5.setServidorIP = "192.250.155.10"
-}catch(error){
-  console.log("ERROR: ", error.message);
-}
-
-
-console.log(GTA5.getServidorIP);
 
